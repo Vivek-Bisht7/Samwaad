e43d4435abb871ffd812b789da7f33c800d04fe8
Vivek <vivekbisht4320@gmail.com>
2025-09-28 22:13:10 +0530

    Initialized websockets(socket.io)

--- server/index.js
+++ server/index.js
@@ -1,30 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {dbConnection} = require('./config/connection');
-const tempUserRoutes = require('./routes/tempUserRoutes');
-const userRoutes = require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const { dbConnection } = require("./config/connection");
+const tempUserRoutes = require("./routes/tempUserRoutes");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
-require('dotenv').config();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+require("dotenv").config();
 dbConnection();
 
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true                
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/otp",tempUserRoutes);
-app.use("/api/user",userRoutes);
-app.use('/api/chat',chatRoutes);
-app.use('/api/message',messageRoutes);
+app.use("/api/otp", tempUserRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
-app.listen(process.env.PORT , ()=>{
-    console.log("Server has started running..");
-})
+server.listen(process.env.PORT, () => {
+  console.log("Server has started running..");
+});
